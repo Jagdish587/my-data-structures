a71266c389f368356fd250917e4aff40bccd1148
Jagdish Tirumala <t.jag587@gmail.com>
2019-02-10 10:10:41 +0530

    added set basics 1

---
+++ set-tutorial/set-basics.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    set<int> myset;
+    myset.insert(10);
+    myset.insert(20);
+    myset.insert(700);
+    myset.insert(50);
+    myset.insert(20);
+    myset.insert(30);
+
+    cout<<"size of set = "<<myset.size()<<endl;
+
+    for(set<int>::iterator it = myset.begin();it !=myset.end();it++)
+        cout<<"value = "<<*it<<endl;
+
+    return 0;
+}
+
